48ab62969098086a5783a4516fd8be38cb77ccde
Olivia Webster <oliviacweb@gmail.com>
2020-04-18 18:29:12 -0400

    Made sample data for tests

---
+++ src/test-data/user-data.js
@@ -0,0 +1,204 @@
+const userData = [
+{
+"id": 1,
+"name": "Leatha Ullrich"
+},
+{
+"id": 2,
+"name": "Rocio Schuster"
+},
+{
+"id": 3,
+"name": "Kelvin Schiller"
+},
+{
+"id": 4,
+"name": "Kennedi Emard"
+},
+{
+"id": 5,
+"name": "Rhiannon Little"
+},
+{
+"id": 6,
+"name": "Fleta Schuppe"
+},
+{
+"id": 7,
+"name": "Dell Rath"
+},
+{
+"id": 8,
+"name": "Era Hand"
+},
+{
+"id": 9,
+"name": "Faustino Quitzon"
+},
+{
+"id": 10,
+"name": "Tony Armstrong"
+},
+{
+"id": 11,
+"name": "Melissa Sauer"
+},
+{
+"id": 12,
+"name": "Kari Keeling"
+},
+{
+"id": 13,
+"name": "Christina Kulas"
+},
+{
+"id": 14,
+"name": "Dallas Schultz"
+},
+{
+"id": 15,
+"name": "Maria Lakin"
+},
+{
+"id": 16,
+"name": "Garry Mills"
+},
+{
+"id": 17,
+"name": "Trudie Grimes"
+},
+{
+"id": 18,
+"name": "Triston Leffler"
+},
+{
+"id": 19,
+"name": "Leatha Hettinger"
+},
+{
+"id": 20,
+"name": "Keon Kirlin"
+},
+{
+"id": 21,
+"name": "Kelsie Rath"
+},
+{
+"id": 22,
+"name": "Amalia Rowe"
+},
+{
+"id": 23,
+"name": "Angus Swift"
+},
+{
+"id": 24,
+"name": "Salvatore Marquardt"
+},
+{
+"id": 25,
+"name": "Rashawn Langworth"
+},
+{
+"id": 26,
+"name": "Antone Olson"
+},
+{
+"id": 27,
+"name": "Sigrid Barrows"
+},
+{
+"id": 28,
+"name": "Isaiah Daniel"
+},
+{
+"id": 29,
+"name": "Maybelle Raynor"
+},
+{
+"id": 30,
+"name": "Devan Mante"
+},
+{
+"id": 31,
+"name": "Deja Bradtke"
+},
+{
+"id": 32,
+"name": "Euna Balistreri"
+},
+{
+"id": 33,
+"name": "Rae Wisozk"
+},
+{
+"id": 34,
+"name": "Ardella Jakubowski"
+},
+{
+"id": 35,
+"name": "Deion Hartmann"
+},
+{
+"id": 36,
+"name": "Michel Kunze"
+},
+{
+"id": 37,
+"name": "Gilda Abernathy"
+},
+{
+"id": 38,
+"name": "Alessia Rippin"
+},
+{
+"id": 39,
+"name": "Moises Nader"
+},
+{
+"id": 40,
+"name": "Leta Lakin"
+},
+{
+"id": 41,
+"name": "Francisca Trantow"
+},
+{
+"id": 42,
+"name": "Freda Bednar"
+},
+{
+"id": 43,
+"name": "Earline Hamill"
+},
+{
+"id": 44,
+"name": "Ruth Ebert"
+},
+{
+"id": 45,
+"name": "Chauncey Dooley"
+},
+{
+"id": 46,
+"name": "Louisa Homenick"
+},
+{
+"id": 47,
+"name": "Rachael Schinner"
+},
+{
+"id": 48,
+"name": "Kaylee Hermann"
+},
+{
+"id": 49,
+"name": "Arnulfo Simonis"
+},
+{
+"id": 50,
+"name": "Eldridge Muller"
+}
+]
+
+export default userData;
