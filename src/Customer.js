bf5bd4a0a8236777126dd332930aa9d6a2153a5e
Olivia Webster <oliviacweb@gmail.com>
2020-04-18 23:33:09 -0400

    Write and pass initial customer tests

--- src/Customer.js
+++ src/Customer.js
@@ -1,10 +1,45 @@
 class Customer {
-  constructor(id, name) {
-    this.id = id
-    this.name = name
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    // this.futureBookings = [];
+    // this.pastBookings = [];
+    // this.totalSpent = 0;
   }
 
+
+  allBookings(bookingData) {
+    return bookingData.filter(booking => booking.userID === this.id).length
+  }
+
+  futureBookings(bookingData, todayDate) {
+  return bookingData.filter(booking => booking.userID === this.id).filter(booking => booking.date > todayDate)
+  }
+
+  pastBookings(bookingData, todayDate) {
+      return bookingData.filter(booking => booking.userID === this.id).filter(booking => booking.date < todayDate)
+  }
+
+  presentBookings(bookingData, todayDate) {
+    return bookingData.filter(booking => booking.userID === this.id).filter(booking => booking.date === todayDate)
+  }
+
+
+
+
+  getTotalSpent(bookingData, roomsData) {
+    let allUserBookings = bookingData.filter(booking => booking.userID === this.id)
+    let currentUserTotal = allUserBookings.reduce((acc, userBooking) => {
+      let roomValue = roomsData.find(room => room.number === userBooking.roomNumber).costPerNight
+      acc += roomValue
+      return acc;
+    }, 0)
+    this.totalSpent = currentUserTotal;
+  }
+
+
 }
+
 
 
 export default Customer;
